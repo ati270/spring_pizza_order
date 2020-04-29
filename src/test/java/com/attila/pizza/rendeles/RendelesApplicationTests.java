eff07d3454cd7ba8f347756c6ce2874e3b5669f6
atii28 <dattila9006@gmail.com>
2020-04-29 10:18:39 +0200

    -- added spring boot project

---
+++ src/test/java/com/attila/pizza/rendeles/RendelesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.attila.pizza.rendeles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RendelesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
